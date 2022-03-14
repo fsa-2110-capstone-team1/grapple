ae3c611b4222611c027e44c1cd3a69bdaf6293d3
bengreenspan <91162661+bengreenspan@users.noreply.github.com>
2022-03-14 11:52:07 -0400

    changed some pages

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 export const Home = () => {
-  return <h1>Grapple</h1>;
+  return <div>
+    <h1>Grapple Home Page to do</h1>
+    <ul>
+<li>
+Ongoing Challenges
+</li>
+<li>
+Scoreboard
+</li>
+<li>
+Progress bar for current challenge
+</li>
+<li>
+Some graph?
+</li>
+    </ul>
+    </div>;
 };
 export default Home;
