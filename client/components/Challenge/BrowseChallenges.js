4efa63554193e41294f3acf32fabafe3b3dd0b9d
bengreenspan <91162661+bengreenspan@users.noreply.github.com>
2022-03-15 15:31:13 -0400

    search working

--- client/components/Challenge/BrowseChallenges.js
+++ client/components/Challenge/BrowseChallenges.js
@@ -1,13 +1,34 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Grid, Box, Button, Typography } from "@mui/material";
 import ChallengeCard from "./ChallengeCard";
+import SearchChallenges from "./SearchChallenges";
+
 
 export const BrowseChallenges = () => {
   const challenges = useSelector((state) => state.challenges);
 
+  // const [order, setOrder] = useState("ASC")
+  // const sorting = (attr) => {
+  //   if (order === "ASC"){
+  //     const sorted = [...challenges].sort((a,b)=>
+  //     a[attr].toLowerCase() > b[attr].toLowerCase() ? 1 : -1
+  //     )
+  //   }
+  // };
+
   return (
     <Grid container>
+      <SearchChallenges data={challenges}/>
+      <div>
+        <h1>Sort by</h1>
+      <div className="sorters">
+      <p onClick={()=>sorting("name")}>Name &nbsp; </p> 
+      <p onClick={()=>sorting("difficulty")}>Difficulty &nbsp;</p>
+      <p onClick={()=>sorting("type")}>Type &nbsp;</p>
+
+      </div>
+      </div>
       <Grid item xs={1} />
       <Grid item xs={10} container>
         {challenges.map((challenge) => (
