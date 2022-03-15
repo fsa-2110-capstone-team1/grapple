de51b4e97f83f3518dc064cc9b92c08bae894f40
Marina Chevis <marinachevis@gmail.com>
2022-03-15 17:42:02 -0400

    seeded 100 users

--- script/seeds/user.js
+++ script/seeds/user.js
@@ -1,39 +1,53 @@
+const axios = require("axios");
 const {
   models: { User },
 } = require("../../server/db");
 
 async function userSeed() {
+  const { data: fakeUsers } = await axios.get(
+    "https://randomuser.me/api/?results=97&inc=login,email,name,picture&seed=grapple&noinfo"
+  );
+
   // Creating Users
   const users = await Promise.all([
     User.create({
-      username: 'cody',
+      username: "cody",
       email: "cody@hello.com",
       password: "123",
       firstName: "Cody",
       lastName: "Williams",
-      isPrivate: true,
       isAdmin: true,
     }),
     User.create({
-      username: 'mary',
+      username: "mary",
       email: "mary@hello.com",
       password: "123",
       firstName: "Mary",
       lastName: "Doe",
-      isPrivate: true,
-      
     }),
     User.create({
-      username: 'john',
+      username: "john",
       email: "john@hello.com",
       password: "123",
       firstName: "John",
       lastName: "Brennan",
-      isPrivate: true,
     }),
   ]);
 
-  console.log(`seeded ${users.length} users`);
+  const fakeUsersCreated = await Promise.all(
+    fakeUsers.results.map((user) =>
+      User.create({
+        username: user.login.username,
+        email: user.email,
+        password: user.login.password,
+        firstName: user.name.first,
+        lastName: user.name.last,
+        image: user.picture.medium,
+      })
+    )
+  );
+
+  console.log(`seeded ${users.length + fakeUsersCreated.length} users`);
 
   return users;
 }
