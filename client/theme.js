72c16cc5e4d30aea62e8b19e947db939e6047c46
Marina Chevis <marinachevis@gmail.com>
2022-03-16 18:04:05 -0400

    browse users

--- client/theme.js
+++ client/theme.js
@@ -1,20 +1,22 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      light: "#D8F1F6",
-      main: "#248DA2",
-      dark: "#196170",
-      contrastText: "#fff",
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: {
+        light: "#D8F1F6",
+        main: "#248DA2",
+        dark: "#196170",
+        contrastText: "#fff",
+      },
+      secondary: {
+        light: "#E0F6FE",
+        main: "#0788B7",
+        dark: "#022B3A",
+        contrastText: "#fff",
+      },
     },
-    secondary: {
-      light: "#E0F6FE",
-      main: "#0788B7",
-      dark: "#022B3A",
-      contrastText: "#fff",
-    },
-  },
-});
+  })
+);
 
 export default theme;
