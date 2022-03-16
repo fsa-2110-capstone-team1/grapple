b305a0e3cc2da52933f7f92121d976a84d5f0066
Louis J Rabeno <lrabeno@gmail.com>
2022-03-16 13:56:52 -0400

    added colors to theme.js

--- client/theme.js
+++ client/theme.js
@@ -2,42 +2,43 @@ import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
   palette: {
+    //Primary is green
     primary: {
       light: '#D8F1F6',
-      main: '#248DA2',
+      // main: '#248DA2',
+      main: '#4AB5A3',
       dark: '#196170',
       contrastText: '#fff',
     },
     secondary: {
-      light: '#E0F6FE',
-      main: '#0788B7',
-      dark: '#022B3A',
+      //secondary is pink
+      light: '#ff80ab',
+      main: '#C54B7B',
+      dark: '#a62859',
       contrastText: '#fff',
     },
-    // warning: {
-    //   light: '#C54B7B',
-    //   main: '#C54B7B',
-    //   dark: '#C54B7B',
-    //   contrastText: '#fff',
-    // },
+    orange: {
+      light: '#edb482',
+      main: '#F57D52',
+      dark: '#db5321',
+      contrastText: '#fff',
+    },
+    yellow: {
+      light: '#f5dda6',
+      main: '#FEBF30',
+      dark: '#e09c00',
+      contrastText: '#fff',
+    },
+    braun: {
+      main: '#271F1C',
+    },
+    grey: {
+      main: '#837873',
+    },
+    white: {
+      main: '#FFFFFF',
+    },
   },
 });
-
-// export const navtheme = createTheme({
-//   palette: {
-//     primary: {
-//       light: '#C54B7B',
-//       main: '#C54B7B',
-//       dark: '#C54B7B',
-//       contrastText: '#fff',
-//     },
-//     secondary: {
-//       light: '#E0F6FE',
-//       main: '#0788B7',
-//       dark: '#022B3A',
-//       contrastText: '#fff',
-//     },
-//   },
-// });
 
 export default theme;
