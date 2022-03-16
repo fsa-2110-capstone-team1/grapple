315fd99f590abde2b9afc575324440e9dc3d0aa8
Louis J Rabeno <lrabeno@gmail.com>
2022-03-16 17:16:19 -0400

    homepage placeholder

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import AuthForm from './Auth/AuthForm';
 
 export const Home = () => {
-  return <div>
-    <h1>Grapple Home Page to do</h1>
-    <ul>
-<li>
-Ongoing Challenges
-</li>
-<li>
-Scoreboard
-</li>
-<li>
-Progress bar for current challenge
-</li>
-<li>
-Some graph?
-</li>
-    </ul>
-    </div>;
+  return (
+    <div>
+      <div>
+        <div className="home-title">
+          <h1>Challenge your friends. Challenge yourself.</h1>
+        </div>
+      </div>
+      <div className="home-text-div">
+        <div className="nother-div">
+          <h4>
+            Welcome to Grapple! The only app where you get serious about your
+            phsyical and mental health. Compete in challenges alone or with a
+            friend to see if you can achieve your goals.
+          </h4>
+        </div>
+      </div>
+      <div className="home-img-div">
+        <img className="home-img" src="homeImgs/grapple-home-runner.jpeg" />
+      </div>
+      <AuthForm />
+    </div>
+  );
 };
 export default Home;
