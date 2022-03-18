bab7bf0f0ce4a9adc8de3048615c079ca6aa55f4
Kate Sv <radistkatya@gmail.com>
2022-03-18 12:14:46 -0700

    userprofile page and user edit form beginning

--- client/components/User/UserProfile.js
+++ client/components/User/UserProfile.js
@@ -1,35 +1,85 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate} from "react-router-dom";
 import {
-  Grid,
-  CartText,
   Card,
-  CardActions,
   CardContent,
   CardMedia,
   Button,
   Typography,
 } from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import dateFormat from "dateformat";
 
 export const UserProfile = () => {
-  const auth = useSelector((state) => state.auth);
-  console.log(auth);
+  const userProfile = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  console.log(userProfile);
 
   return (
-    <div className="profile">
-      <img
-        className="profile-header"
-        src="/homeImgs/grapple-home-runner.jpeg"
-        alt="background-image"
-      />
-
-      <i
-        className="material-icons"
-        id="profile-image"
-        style={{ marginTop: "20px", fontSize: "155px", color: "#dfdfdf" }}
-      >
-        face
-      </i>
+    <div className="profile-container">
+      <Card className="profile">
+        <CardMedia className="image-stack">
+          <img
+            id="profile-header"
+            src="/homeImgs/grapple-home-runner.jpeg"
+            alt="background-image"
+          />
+          {userProfile.image ? (
+            <img id="profile-image" src={userProfile.image} />
+          ) : (
+            <i
+              className="material-icons"
+              id="profile-image"
+              style={{
+                marginTop: "20px",
+                fontSize: "155px",
+                color: "#dfdfdf",
+              }}
+            >
+              face
+            </i>
+          )}
+        </CardMedia>
+        <CardContent>
+          
+            <Typography
+              className="profile-title"
+              gutterBottom
+              variant="h5"
+              component="div"
+            >
+              {userProfile.firstName} {userProfile.lastName}
+            </Typography>
+            <List className="profile-list">
+              <ListItem
+                key={userProfile.email}
+              >
+                <ListItemText primary={`Your email: ${userProfile.email}`} />
+              </ListItem>
+              <ListItem key={userProfile.username}>
+                <ListItemText
+                  primary={`Your username: ${userProfile.username}`}
+                />
+              </ListItem>
+              <ListItem key={userProfile.createdAt}>
+                <ListItemText
+                  primary={`Member since: ${dateFormat(userProfile.createdAt, "mediumDate")}`}
+                />
+              </ListItem>
+            </List>
+            <Button
+              size="medium"
+              fullWidth
+              variant="contained"
+              onClick={() => navigate(`/user/profile/edit`)}
+            >
+              Edit Profile
+            </Button>
+        </CardContent>
+      </Card>
     </div>
   );
 };
