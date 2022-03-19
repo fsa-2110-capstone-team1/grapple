9298edfb11692ef20778f5682f24b20525dd1dfb
Louis J Rabeno <lrabeno@gmail.com>
2022-03-18 22:52:51 -0400

    worked on challengeDetails.js

--- client/components/Challenge/ChallengeDetails.js
+++ client/components/Challenge/ChallengeDetails.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
 import {
   Grid,
   Card,
@@ -9,55 +9,101 @@ import {
   CardMedia,
   Button,
   Typography,
-} from "@mui/material";
-import { getAllChallenges } from "../../store";
+  Box,
+  Paper,
+  styled,
+} from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../../theme';
+import { getAllChallenges } from '../../store';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark' ? '#1A2027' : theme.palette.secondary.main,
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.braun,
+}));
 
 export const ChallengeDetails = () => {
-  const state = useSelector((state) => state);
+  const challenges = useSelector((state) => state.challenges);
+  // const state = useSelector((state) => state);
+  // console.log('STATE', state);
 
   const { id } = useParams();
-  const challenge = state.challenges.find(
-    (challenge) => challenge.id === id * 1
-  );
 
-  console.log("challenge", challenge);
+  const userChallenges = useSelector((state) => state.userChallenges);
+  const currUserChall =
+    userChallenges.filter((userChallenge) => userChallenge.userId === id * 1) ||
+    [];
+
+  const challenge =
+    challenges.find((challenge) => challenge.id === id * 1) || {};
+
+  console.log('challenge', challenge);
   if (!challenge) {
-    return "Sorry the challenge you are looking for is unreachable";
+    return 'Sorry the challenge you are looking for is unreachable';
   }
 
   return (
-    <Card
-      sx={{
-        maxWidth: 345,
-        // Provide some spacing between cards
-        margin: 1.5,
-        // Use flex layout with column direction for components in the card
-        // (CardContent and CardActions)
-        display: "flex",
-        flexDirection: "column",
-        // Justify the content so that CardContent will always be at the top of the card,
-        // and CardActions will be at the bottom
-        justifyContent: "space-between",
-      }}
-    >
-      <CardMedia
-        component="img"
-        height="200"
-        image={`/${challenge.image}`}
-        alt="challenge cover photo"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {challenge.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" height="auto">
-          {challenge.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Join Challenge</Button>
-      </CardActions>
-    </Card>
+    <ThemeProvider theme={theme}>
+      <div>
+        <div className="meditate-img-div">
+          {/* <img
+          className="meditate-img"
+          src="../homeImgs/grapple-cycle-group.jpeg"
+        /> */}
+        </div>
+        <div className="single-challenge-div">
+          <Box sx={{ width: 1 }}>
+            <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+              <Box gridColumn="span 12">
+                <Item xs={12}>
+                  <h1>Challenge: {challenge.name}</h1>
+                </Item>
+              </Box>
+              <Box gridColumn="span 12">
+                <Item>
+                  <h2>Difficulty Rating: {challenge.difficulty}</h2>
+                </Item>
+              </Box>
+              <Box gridColumn="span 12">
+                <Item>
+                  {' '}
+                  <h2>{challenge.description}</h2>
+                </Item>
+              </Box>
+              <Box gridColumn="span 6">
+                <Item>
+                  <h4>Start Date: {challenge.startDateTime}</h4>
+                </Item>
+              </Box>
+              <Box gridColumn="span 6">
+                <Item>
+                  <h4>End Date: {challenge.endDateTime}</h4>
+                </Item>
+              </Box>
+              <Box gridColumn="span 12">
+                <Item>
+                  <h4>
+                    {currUserChall.length === 0
+                      ? 'No one currently enrolled in this challenge :('
+                      : currUserChall.length === 1
+                      ? `There is only ${currUserChall.length} person enrolled in this challenge!`
+                      : ` There are ${currUserChall.length} people enrolled in this challenge!`}
+                  </h4>
+                </Item>
+              </Box>
+            </Box>
+          </Box>
+          <div className="single-challenge-bttns">
+            <Button size="large">Join Challenge</Button>
+            <Button size="large">Invite A friend</Button>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 };
 export default ChallengeDetails;
